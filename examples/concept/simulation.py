484325fa8a341fc65fa4228f2bc750f3d15beb18
Patrick Kage <quadnix1@gmail.com>
2018-01-17 18:32:00 +0000

    reverted concept to original concept code

--- examples/concept/simulation.py
+++ examples/concept/simulation.py
@@ -10,11 +10,14 @@
 
 # import the crux libraries
 from crux.client import CruxClient as Crux
-#from yourcode import Simulation, SimulationException
+from yourcode import Simulation, SimulationException
 
 if __name__ == "__main__":
     # initialize everything
     cc = Crux('crux.json')
+
+    # connect to the crux backend (sends the description files to the backend)
+    cc.connect()
 
     while True:
         # wait for the backend to send some data for processing
@@ -28,11 +31,13 @@ if __name__ == "__main__":
             sim = Simulation(**config)
 
             # do some operation to the data
-            # output = sim.do_some_simulation(data)
-            output = 'foo'
+            output = sim.do_some_simulation(data)
 
             # output that data back to the backend
             cc.output(output)
+        except SimulationException ex:
+            # handle an error state gracefully
+            cc.fail(ex.message)
         except:
             # hard failure
             cc.fail()
