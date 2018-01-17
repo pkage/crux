3ae52a672e2cce75eef449c3a4af81fa1d0d6907
Patrick Kage <quadnix1@gmail.com>
2018-01-17 22:21:07 +0000

    add example client test target

---
+++ examples/client/simulation.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+
+##
+# Crux example component
+# @author Patrick Kage
+# 
+# This is an example single-file simulation component
+# this is using an example version of the crux client library
+# Note that the API may change in future!
+
+# import the crux libraries
+from crux.client import CruxClient as Crux
+#from yourcode import Simulation, SimulationException
+
+if __name__ == "__main__":
+    # initialize everything
+    cc = Crux('crux.json')
+
+    while True:
+        # wait for the backend to send some data for processing
+        data, config, done = cc.wait()
+
+        if done:
+            break
+
+        try:
+            # create the simulation from the configuration sent over
+            # sim = Simulation(**config)
+
+            # do some operation to the data
+            # output = sim.do_some_simulation(data)
+            output = 'foo'
+
+            # output that data back to the backend
+            cc.output(output)
+        except:
+            # hard failure
+            cc.fail()
+    # do cleanup here if necessary
