08ca945f843c3bad267be9892f36cce6f26725ba
Patrick Kage <quadnix1@gmail.com>
2018-01-21 19:20:24 +0000

    WIP validation layer

--- crux/common/validation.py
+++ crux/common/validation.py
@@ -2,4 +2,17 @@
 # Crux data validation
 # @author Patrick Kage
 
+import semver
+import jsonschema
 
+def version_check(candidate, constraint):
+    """Ensure that a candidate component version matches a constraint (semver)
+
+    :param candidate: the candidate to drop in
+    :param constraint: version constraint
+    """
+    return semver.match(candidate, constraint)
+
+def validate_schema(schema, target):
+    # not a solution
+    return True
