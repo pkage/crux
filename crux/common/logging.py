afc1653b487b2f85c3e16ad206fc121a64b8b304
Patrick Kage <quadnix1@gmail.com>
2018-01-23 08:53:59 +0000

    update logger to include names in output

--- crux/common/logging.py
+++ crux/common/logging.py
@@ -8,10 +8,21 @@ from termcolor import colored
 class Logger:
     logging = False
     filt = None
+    __name = None
 
-    def __init__(self, logging=False, filt=None):
+    def __init__(self, logging=False, filt=None, name=None):
         self.logging = logging
         self.filt = filt
+        self.set_name(name)
+
+    def set_name(self, name):
+        if name is not None:
+            self.__name = name
+        else:
+            self.__name = 'log'
+
+        # python's formatter doesn't handle non-printing characters, so pre-baking padding
+        self.__name = colored('{:8.8}'.format(self.__name), 'magenta')
 
     def log(self, text, level='debug'):
         if not self.logging or self.__filtered(level):
@@ -27,7 +38,7 @@ class Logger:
         else:
             level = colored('info ', 'green')
 
-        print( '[{}]: {}'.format(level, text) )
+        print( '[{}][{}]: {}'.format(self.__name, level, text) )
 
     def __call__(self, text, level='debug'):
         self.log(text, level)
