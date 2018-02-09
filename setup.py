f5c93d7d4630b5512fb87db01c04b892f60a54c7
Patrick Kage <quadnix1@gmail.com>
2018-02-09 03:03:13 +0000

    ensure setup includes all web files, including subdirectories

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup
+
+# quick hack to recursively find all web files
+def package_files(directory, remove):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join(path, filename).replace(remove, '', 1))
+    return paths
 
 setup(name='crux',
       version='0.0.1',
@@ -35,7 +44,7 @@ setup(name='crux',
           ]
       },
       package_data={
-          'crux.wizards.web': ['static/*']
+          'crux.wizards.web': package_files('crux/wizards/web/static/', 'crux/wizards/web/')
       },
       include_package_data=True
      )
