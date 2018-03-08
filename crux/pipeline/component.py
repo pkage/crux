9c0e33d59250079cef3614aaec724164da924aa1
Patrick Kage <quadnix1@gmail.com>
2018-03-08 01:26:34 +0000

    convert base Component class to use ManagedSocket

--- crux/pipeline/component.py
+++ crux/pipeline/component.py
@@ -4,6 +4,7 @@
 
 import json
 import zmq
+from crux.common.socket import ManagedSocket
 from crux.common.logging import Logger
 from crux.common.messaging import Message
 
@@ -21,11 +22,12 @@ class Component:
     # housekeeping
     __log     = None
 
-    def __init__(self, address, socket=None, context=None):
+    def __init__(self, address, socket=None, context=None, timeout=None):
         """Initialize the component
 
         :param address: the address of the component
         :param socket: the socket to use for communication, if we're sharing (one will be created if not provided)
+        :param timeout: timeout for resolving the component
         :param context: the context to use to create a socket (if no socket provided), one will be created if not used
         """
         # logging?
@@ -40,25 +42,22 @@ class Component:
             self.__context = context
 
         # if we haven't got a socket yet, get one
-        if socket is None:
-            self.__socket = self.__context.socket(zmq.REQ)
-        else:
-            self.__socket = socket
+        self.__socket = ManagedSocket(self.__context, zmq.REQ)
 
         # connect the socket
         self.__log('connecting to {}'.format(address))
         self.__socket.connect(address)
 
         # get the cruxfile
-        self.cruxfile = self.request(Message(name='get_cruxfile')).payload
+        self.cruxfile = self.request(Message(name='get_cruxfile'), timeout=timeout).payload
 
-    def request(self, msg):
+    def request(self, msg, timeout=None):
         """Do a request on this component
 
         :param msg: the message to post to the client
+        :param timeout: timeout in ms
         :returns: the reply
         """
-        self.__socket.send(msg.pack())
-        return Message(data=self.__socket.recv())
+        return self.__socket.call(msg, timeout=timeout)
 
 
